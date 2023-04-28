de37a83edf2d4b6b74a41c2994c664f1b7123963
Wendril-Costa <wendrild.c.n@gmail.com>
2023-04-28 20:02:04 -0300

    Requisito 3 Feito - Classe CompleteReport concluido utilizando o defaultdict

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from collections import defaultdict
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products):
+        report = SimpleReport.generate(products)
+        companies = defaultdict(int)
+
+        for product in products:
+            companies[product["nome_da_empresa"]] += 1
+
+        report += "\nProdutos estocados por empresa:\n"
+
+        for company, count in companies.items():
+            report += f"- {company}: {count}\n"
+
+        return report
