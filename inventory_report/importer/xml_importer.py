4a1abecfa712f142e0a5cde15970e1ef30533e4a
Wendril-Costa <wendrild.c.n@gmail.com>
2023-04-29 18:31:39 -0300

    Tentando de novo

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,13 @@
+import xmltodict
+from inventory_report.importer.importer import Importer
+
+
+class XmlImporter(Importer):
+    @staticmethod
+    def import_data(file_path):
+        if not file_path.endswith('.xml'):
+            raise ValueError('Arquivo inválido')
+
+        with open(file_path, "r") as file:
+            products = xmltodict.parse(file.read())["dataset"]["record"]
+        return products
--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,6 @@
-import csv
-import json
-import xmltodict
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -8,7 +8,7 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @staticmethod
     def import_data(file_path, report_type):
-        products = Inventory._get_products(file_path)
+        products = Inventory.get_products(file_path)
 
         if report_type == "simples":
             report = SimpleReport.generate(products)
@@ -18,13 +18,13 @@ class Inventory:
         return report
 
     @staticmethod
-    def _get_products(file_path):
+    def get_products(file_path):
         extension = file_path.split(".")[-1]
-        with open(file_path, "r") as file:
-            if extension == "csv":
-                products = list(csv.DictReader(file))
-            elif extension == "json":
-                products = json.load(file)
-            elif extension == "xml":
-                products = xmltodict.parse(file.read())["dataset"]["record"]
-        return products
+
+        if extension == "csv":
+            products = CsvImporter()
+        elif extension == "json":
+            products = JsonImporter()
+        elif extension == "xml":
+            products = XmlImporter()
+        return products.import_data(file_path)
