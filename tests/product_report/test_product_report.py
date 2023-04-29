d04056051a6bf7c8ce84a4128422bafd39a6d239
Wendril-Costa <wendrild.c.n@gmail.com>
2023-04-29 19:53:20 -0300

    Requisito 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+import datetime
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=2,
+        nome_do_produto="arroz",
+        nome_da_empresa="Arrozeiro",
+        data_de_fabricacao=datetime.date(2022, 1, 1),
+        data_de_validade=datetime.date(2024, 1, 1),
+        numero_de_serie="AR123",
+        instrucoes_de_armazenamento="em local seco e fresco",
+    )
+
+    assert str(product) == (
+        "O produto arroz"
+        " fabricado em 2022-01-01"
+        " por Arrozeiro com validade"
+        " até 2024-01-01"
+        " precisa ser armazenado em local seco e fresco."
+    )
